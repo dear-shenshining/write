49cc6d5a6f89ae8e1fd9c836cd3e4449a7d2eb0d
Administrator <1426225727@qq.com>
2026-06-02 13:35:00 +0800

    修改

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,23 +12,9 @@ const notoSerifSC = Noto_Serif_SC({
 export const metadata: Metadata = {
   title: '致因爱创作的你们 | 感谢信',
   description: '一封来自"一间属于自己的房间"的感谢信，致敬为128名珍珠生捐款的每一位善心人。',
-  generator: 'v0.app',
   icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
+    icon: '/icon.svg',
+    apple: '/icon.svg',
   },
 }
 
